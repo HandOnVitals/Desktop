ec35b03afbed7c3e74f5011681ccebdb0a108dc5
Alexandre Serra <45828914+amserra@users.noreply.github.com>
2022-06-09 15:33:32 +0100

    Updated boilerplate

---
+++ src/main/java/hov/SecondaryController.java
@@ -0,0 +1,12 @@
+package hov;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+
+public class SecondaryController {
+
+    @FXML
+    private void switchToPrimary() throws IOException {
+        App.setRoot("primary");
+    }
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,5 +1,8 @@
 module hov {
     requires javafx.controls;
+    requires javafx.fxml;
+
+    opens hov to javafx.fxml;
     requires pt.gov.cartaodecidadao;
     exports hov;
 }
